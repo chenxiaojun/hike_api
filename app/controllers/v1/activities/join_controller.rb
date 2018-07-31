2179f3e7f715711f951db75b3e8f31a38b507f1f
Ricky <rickycxj@gmail.com>
2018-07-31 12:20:04 +0800

    添加审核通过用户报名接口

--- app/controllers/v1/activities/join_controller.rb
+++ app/controllers/v1/activities/join_controller.rb
@@ -24,7 +24,9 @@ module V1
       def update
         @member = @activity.activity_joins.find(params[:id])
         requires! :status, values: %w[passed failed]
+        raise_error 'cannot_change_member_status' if @member.join_status.eql?('passed') # 报名通过的用户不能再次操作
         @member.update(join_status: params[:status], response_message: params[:message])
+        @activity.increment_join_numbers if params[:status].eql?('passed')
         render_api_success
       end
 
